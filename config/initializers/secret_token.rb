3abc3bbaa5f8b66878a6d81d67aaa204f3f2e752
William Herry <WilliamHerryChina@Gmail.com>
2014-03-10 23:17:44 +0800

    initialize commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AnimalIdentificationSystem::Application.config.secret_key_base = 'df7eed42d3cc2b9509b98efda4d691affdf2ebdc047f734740ec95c351e9b281a2c7a89eddc4c1f06884ebe7fb4473a6c07a342b80cc277a4e20b2f688d9c07b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AnimalIdentificationSystem::Application.config.session_store :cookie_store, key: '_animal-identification-system_session'
